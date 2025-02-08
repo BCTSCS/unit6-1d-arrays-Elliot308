762eac72c38ee75404210e3649da79715953c2e9
Elliot308 <elllim25@bergen.org>
2025-02-07 21:25:18 -0500

    fileoperater.java created

---
+++ FileOperator.java
@@ -0,0 +1,11 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class FileOperator {
+    //private Scanner fileReader;
+    //private File myFile;
+    //toStringArray(arraySize)
+    //toIntArray(arraySize)   
+    //toDoubleArray(arraySize)
+
+}
